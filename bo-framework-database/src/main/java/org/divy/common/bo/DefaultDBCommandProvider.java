8238e8036ead25caa6f9be2a0ea4ea3e8507fb04
divyakumar <divyakumarjain@gmail.com>
2013-04-14 18:13:39 +0530

    Improve code based on sonar feedback

--- bo-framework-database/src/main/java/org/divy/common/bo/DefaultDBCommandProvider.java
+++ bo-framework-database/src/main/java/org/divy/common/bo/DefaultDBCommandProvider.java
@@ -25,24 +25,18 @@ public class DefaultDBCommandProvider<ENTITY extends IBusinessObject<ID>, ID>
 		extends
 		TypeBaseDBCommandProvider<ENTITY, ID> {
 
-	/**
-	 * 
-	 */
 	private static final String COULD_NOT_CREATE_COMMAND = "Could not Create Command";
 
-	/**
-	 * @param persistantUnitName
-	 */
 	@SuppressWarnings("unchecked")
 	public DefaultDBCommandProvider(String persistantUnitName,
 			Class<ENTITY> entityClass) {
 		super(persistantUnitName);
 
-		setGetCommandType((Class<? extends IGetCommand<ENTITY, ID>>) DefaultDatabaseGetCommand.class);
-		setCreateCommandType((Class<? extends ICreateCommand<ENTITY, ID>>) DefaultDatabaseCreateCommand.class);
-		setDeleteCommandType((Class<? extends IDeleteCommand<ENTITY, ID>>) DefaultDatabaseDeleteCommand.class);
-		setSearchCommandType((Class<? extends ISearchCommand<ENTITY, ID>>) DefaultDatabaseSearchCommand.class);
-		setUpdateCommandType((Class<? extends IUpdateCommand<ENTITY, ID>>) DefaultDatabaseUpdateCommand.class);
+		setGetCommandType((Class<? extends IGetCommand<ENTITY, ID>>)(Object) DefaultDatabaseGetCommand.class);
+		setCreateCommandType((Class<? extends ICreateCommand<ENTITY, ID>>)(Object) DefaultDatabaseCreateCommand.class);
+		setDeleteCommandType((Class<? extends IDeleteCommand<ENTITY, ID>>)(Object) DefaultDatabaseDeleteCommand.class);
+		setSearchCommandType((Class<? extends ISearchCommand<ENTITY, ID>>)(Object) DefaultDatabaseSearchCommand.class);
+		setUpdateCommandType((Class<? extends IUpdateCommand<ENTITY, ID>>)(Object) DefaultDatabaseUpdateCommand.class);
 
 		this.entityClass = entityClass;
 	}
