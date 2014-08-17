53404f923677ca9137c42c96b0fa2e6047ad99dc
divyakumarjain <divyakumarjain@gmail.com>
2014-08-17 17:02:00 +0530

    Update Maven structure

--- bo-framework-presentation/src/main/java/org/divy/common/bo/presentation/navigation/IBOApplicationSection.java
+++ bo-framework-presentation/src/main/java/org/divy/common/bo/presentation/navigation/IBOApplicationSection.java
@@ -1,25 +1,7 @@
 package org.divy.common.bo.presentation.navigation;
 
-import java.util.List;
 
-public class IBOApplicationSection implements IApplicationSection {
+public interface IBOApplicationSection extends IApplicationSection {
 
-	@Override
-	public String getSectionLabel() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<IApplicationSection> getSubSection() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<IOperation> getOperations() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
