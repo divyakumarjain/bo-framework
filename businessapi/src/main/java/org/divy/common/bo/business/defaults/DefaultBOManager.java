7471eb7a4812a946af3dd33c28c5e94c5ab04bb1
divyakumarjain <divyakumarjain@gmail.com>
2016-12-06 06:00:55 +0530

    Manage Dependencies using Spring BOM

--- businessapi/src/main/java/org/divy/common/bo/business/defaults/DefaultBOManager.java
+++ businessapi/src/main/java/org/divy/common/bo/business/defaults/DefaultBOManager.java
@@ -1,6 +1,5 @@
 package org.divy.common.bo.business.defaults;
 
-import javax.inject.Inject;
 
 import org.divy.common.bo.IBORepository;
 import org.divy.common.bo.IBusinessObject;
@@ -9,7 +8,6 @@ import org.divy.common.bo.business.AbstractBOManager;
 
 public class DefaultBOManager<ENTITY extends IBusinessObject<ID>, ID> extends AbstractBOManager<ENTITY, ID> {
 
-    @Inject
     public DefaultBOManager(IBORepository<ENTITY, ID> repository) {
         super(repository);
     }
