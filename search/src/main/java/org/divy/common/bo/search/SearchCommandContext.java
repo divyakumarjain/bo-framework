f08120c8aa09b97a734db94a1e1574b3699b8cb7
divyakumarjain <divyakumarjain@gmail.com>
2020-12-22 18:10:06 +0530

    Remove Hibernate Search

--- search/src/main/java/org/divy/common/bo/search/SearchCommandContext.java
+++
@@ -1,6 +0,0 @@
-package org.divy.common.bo.search;
-
-import org.divy.common.bo.database.context.HierarchicalCommandContext;
-
-public interface SearchCommandContext extends HierarchicalCommandContext {
-}
