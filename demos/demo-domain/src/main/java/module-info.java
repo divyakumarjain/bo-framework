a60a9e5a69b55e234fb415c84e13add4140f355d
divyakumarjain <divyakumarjain@gmail.com>
2020-12-27 01:33:03 +0530

    WIP jakarta bridge

--- demos/demo-domain/src/main/java/module-info.java
+++ demos/demo-domain/src/main/java/module-info.java
@@ -1,9 +1,8 @@
 module bo.framework.demo.domain {
     requires bo.framework.database.jpa;
-    requires jakarta.persistence;
     requires org.hibernate.orm.core;
     requires bo.framework.repository;
-    requires javax.persistence;
+    requires jakarta.persistence;
     opens org.divy.bo.demos.domain.greetings;
     opens org.divy.bo.demos.domain.cards.pages;
 
