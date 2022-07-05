95eb8ded67e0ff3cca624f8f2a742b357a5be0e2
Divyakumar Jain <divyakj@amazon.com>
2022-07-06 00:09:03 +0530

    Remove Deprecation for Database command pattern

--- database-root/database-core/src/main/java/org/divy/common/bo/database/command/CommandProvider.java
+++ database-root/database-core/src/main/java/org/divy/common/bo/database/command/CommandProvider.java
@@ -5,9 +5,7 @@ package org.divy.common.bo.database.command;
  * @param <E> Entity for which persistence operation are needed
  * @param <I> Identity of the entity
  *
- * @deprecated Please use Dependency injection framework
  */
-@Deprecated
 public interface CommandProvider<E, I>
 {
 
